007c3a9677f15d57138c336260ded7190e3e3c15
Steve Jia <sxsjia@gmail.com>
2018-08-13 16:23:07 -0500

    add solution for R2

--- R2/Program.cs
+++ R2/Program.cs
@@ -11,7 +11,14 @@ namespace R2
     {
         static void Main(string[] args)
         {
-            
+            string line = Console.ReadLine();
+            if(string.IsNullOrEmpty(line)){return;}
+
+            int[] inputs = Array.ConvertAll(line.Split(' '), Int32.Parse);
+            int R1 = inputs[0];
+            int S = inputs[1];
+
+            Console.WriteLine(S*2 - R1);
         }
     }
 }
