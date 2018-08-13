7a6941cc121e252c2caf83a269a8ed3ee373f70b
Steve Jia <sxsjia@gmail.com>
2018-08-13 16:43:36 -0500

    added solution to QuadrantSelection

---
+++ QuadrantSelection/Program.cs
@@ -0,0 +1,26 @@
+﻿/******
+ * Quadrant Selection
+ * https://open.kattis.com/problems/quadrant
+ * Difficulty - 1.2
+ ******/
+using System;
+using System.Linq;
+
+namespace QuadrantSelection
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] inputs = new int[2];
+            string line;
+            for(int i = 0; i < 2 && ((line = Console.ReadLine()) != null); i++){
+                inputs[i] = Int32.Parse(line);
+            }
+
+            if(inputs.Any(i => i == 0)){return;}
+
+            Console.WriteLine(inputs[0] > 0 ? (inputs[1] > 0 ? 1 : 4) : (inputs[1] > 0 ? 2 : 3));
+        }
+    }
+}
