6126efaa6be848c149d5e60e3f17148374784aed
Steve Jia <sxsjia@gmail.com>
2018-08-13 16:18:21 -0500

    added R2 project files

---
+++ R2/Program.cs
@@ -0,0 +1,17 @@
+﻿/******
+ * R2
+ * https://open.kattis.com/problems/r2
+ * Difficulty - 1.2
+ ******/
+using System;
+
+namespace R2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+        }
+    }
+}
