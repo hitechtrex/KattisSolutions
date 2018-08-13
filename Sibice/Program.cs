8d553c0c8eae8154a7c473fdbca75c46c8343870
Steve Jia <sxsjia@gmail.com>
2018-08-13 15:18:50 -0500

    fixed some SLN stuff

---
+++ Sibice/Program.cs
@@ -0,0 +1,17 @@
+﻿/******
+ * Sibice
+ * https://open.kattis.com/problems/sibice
+ * Difficulty - 1.2
+ ******/
+using System;
+
+namespace Sibice
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
